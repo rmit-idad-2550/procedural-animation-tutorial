b6355fa28595c380aeadd4e59d49ad2f97c18731
Patrick McMahon <patrick.mcmahon@rmit.edu.au>
2025-10-01 15:21:47 +1000

    week9

--- script.js
+++ script.js
@@ -9,12 +9,52 @@ let aBThree = document.getElementById("animButtonThree");
 aBThree.addEventListener("click", animateThree);
 
 function animateOne() {
-  //
+  // check current animation state
+  if(circle.style.animationPlayState === "paused"){
+    // we want to start animation playback when clicked
+    circle.style.animationPlayState = "running";
+  } else {
+    // we want to start animation playback when clicked
+    circle.style.animationPlayState = "paused";
+  }
+  
 }
 
 /* https://developer.mozilla.org/en-US/docs/Web/API/Web_Animations_API/Using_the_Web_Animations_API */
 function animateTwo() {
-  //
+  // create animation from scratch
+  let animTiming = {
+    duration: 3000,
+    easing: "ease-in",
+    iterations: Infinity
+  };
+  // create the keyframes
+  let animFrames = [
+    {transfrom: "translateX(10vh)", scale: 3},
+    {transfrom: "translateX(-10vh)", scale: 2},
+    {transfrom: "translateX(3vh)", scale: 1},
+    {transfrom: "translateX(-6vh)", scale: 3}
+  ];
+  circle.animate(animFrames, animTiming);
+}
+
+function animateThree() {
+  // generate random modifier
+  let randomMod = Math.random();
+  // create animation from scratch
+  let animTiming = {
+    duration: 3000 * randomMod,
+    easing: "ease-in",
+    iterations: Infinity
+  };
+  // create the keyframes
+  let animFrames = [
+    {transfrom: `translateX(${10 * randomMod}vh)`, scale: 3},
+    {transfrom: "translateX(-10vh)", scale: 2},
+    {transfrom: "translateX(3vh)", scale: 1},
+    {transfrom: "translateX(-6vh)", scale: 3}
+  ];
+  circle.animate(animFrames, animTiming);
 }
 
 function animateFour() {
